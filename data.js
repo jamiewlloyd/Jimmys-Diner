91593118140ae56d70b4ac0f24668437d9e06fc3
Jamie Lloyd <jamielloyd.work@gmail.com>
2026-08-19 11:33:10 +0200

    Initial project setup

---
+++ data.js
@@ -0,0 +1,23 @@
+const menuArray = [
+   {
+      name: "Pizza",
+      ingredients: ["pepperoni", "mushroom", "mozzarella"],
+      price: 14,
+      emoji: "🍕",
+      id: 0
+   },
+   {
+      name: "Hamburger",
+      ingredients: ["beef", "cheese", "lettuce"],
+      price: 12,
+      emoji: "🍔",
+      id: 1
+   },
+   {
+      name: "Beer",
+      ingredients: ["grain, hops, yeast, water"],
+      price: 12,
+      emoji: "🍺",
+      id: 2
+   }
+]
